17930f6f9b3dc31402abc013c8b0d9c163f689bc
Crowdbotics <team@crowdbotics.com>
2023-07-18 09:05:50 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "small-butterfly-42246",
+  url: "https://small-butterfly-42246.botics.co",
+  api: "https://small-butterfly-42246.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
